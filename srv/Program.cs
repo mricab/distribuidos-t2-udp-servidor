e4eae346b30efe596e450c48f6e9b6edd40fc4bf
Marcelo Rios <chelo38@gmail.com>
2021-05-11 11:53:05 -0400

    Basic server implementation usign UdpClient class

---
+++ srv/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace srv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Greeting
+            Console.WriteLine("Waiting for connections.");
+
+            // Socket initilization
+            UdpClient client = new UdpClient(8080);
+            IPEndPoint remoteip = new IPEndPoint(IPAddress.Any,8080);
+
+            // Stream
+            byte[] receivedBytes = client.Receive(ref remoteip);
+            if(receivedBytes != null) {
+                string message = Encoding.ASCII.GetString(receivedBytes);
+                Console.WriteLine("Message received: {0}", message);
+            } else {
+                Console.WriteLine("Empty Message Received.");
+            }
+
+            // Idle mode
+            Console.ReadLine();
+        }
+    }
+}
